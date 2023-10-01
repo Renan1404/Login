a5a9d99d66cf38b0f9b264446a9a4954e2eeb229
Renan1404 <82480715+Renan1404@users.noreply.github.com>
2023-09-30 23:54:44 -0300

    Add files via upload

---
+++ node-login/src/app/Auth/controllers/AuthController.ts
@@ -0,0 +1,9 @@
+class AuthController {
+    async create(req: Request, res: Response): Promise<Response> {
+        return res.status(200).json({ message: 'Hello World' })
+    }
+
+    async destroy() {}
+}
+
+export default new AuthController()
